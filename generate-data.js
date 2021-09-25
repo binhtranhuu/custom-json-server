9f588a1456f090c1e8bbe98e000b7816f9fdb73b
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-09-25 08:34:50 +0700

    Random dữ liệu cho categories với faker.js

--- generate-data.js
+++ generate-data.js
@@ -1,7 +1,41 @@
 var faker = require("faker");
+var fs = require("fs");
 
 faker.locale = "vi";
 
-console.log(faker.name.findName());
-console.log(faker.internet.email());
-console.log(faker.helpers.createCard());
+const randomCategoryList = (n) => {
+  if (n < 0) return [];
+  const categoryList = [];
+
+  Array.from(new Array(n)).forEach(() => {
+    const category = {
+      id: faker.datatype.uuid(),
+      name: faker.commerce.department(),
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+
+    categoryList.push(category);
+  });
+
+  return categoryList;
+};
+
+(() => {
+  // Random data
+  const categoryList = randomCategoryList(5);
+
+  //Prepare db object
+  const db = {
+    categories: categoryList,
+    products: [],
+    profile: {
+      name: "Binh tran",
+    },
+  };
+
+  // Write db object to db.json
+  fs.writeFile("db.json", JSON.stringify(db), () => {
+    console.log("Generate data successfully");
+  });
+})();
