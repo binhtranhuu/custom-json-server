6c2149a0be221130a9fe224def8f2bd721c7881a
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-09-25 09:17:58 +0700

    Random sản phẩm theo từng danh mục với Faker.js

--- generate-data.js
+++ generate-data.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 faker.locale = "vi";
 
 const randomCategoryList = (n) => {
-  if (n < 0) return [];
+  if (n <= 0) return [];
   const categoryList = [];
 
   Array.from(new Array(n)).forEach(() => {
@@ -21,14 +21,40 @@ const randomCategoryList = (n) => {
   return categoryList;
 };
 
+const randomProductList = (categoryList, numberOfProduct) => {
+  if (numberOfProduct <= 0) return [];
+  const productList = [];
+
+  for (let category of categoryList) {
+    Array.from(new Array(numberOfProduct)).forEach(() => {
+      const product = {
+        categoryId: category.id,
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        color: faker.commerce.color(),
+        price: faker.commerce.price(),
+        description: faker.commerce.productDescription(),
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        thumbnailUrl: faker.image.imageUrl(400, 400),
+      };
+
+      productList.push(product);
+    });
+  }
+
+  return productList;
+};
+
 (() => {
   // Random data
   const categoryList = randomCategoryList(5);
+  const productList = randomProductList(categoryList, 5);
 
   //Prepare db object
   const db = {
     categories: categoryList,
-    products: [],
+    products: productList,
     profile: {
       name: "Binh tran",
     },
